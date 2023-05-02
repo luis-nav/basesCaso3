c6ff4c9906e7c8497226e4731e0eaa42c91597d2
Victoria Sandí <vsandi@estudiantec.cr>
2023-05-01 21:11:01 -0600

    Creacion, llenado y tvp

---
+++ Llenado/llenadoBasico.sql
@@ -0,0 +1,5 @@
+insert into UnidadesDeMedidas (Unidad, Acronimo) values ('Litros', 'L');
+
+insert into TiposResiduos (Tipo) values ('Liquido');
+
+insert into Residuos (Nombre, TipoResiduoID, UnidadDeMedidaID) values ('Aguas Jabonosas', 1, 1);
---
+++ TVP.sql
@@ -0,0 +1,79 @@
+-- Author: Victoria Sandí Barrantes
+-- Fecha: 26-04-2021
+-- Desc: procedimiento almacenado 
+-----------------------------------------------------------
+
+
+CREATE TYPE NuevoContrato 
+   AS TABLE
+      ( InicioVigencia DATE,
+		FinalVigencia DATE,
+		ProductorID INT,
+		CostoContratoBruto DECIMAL(18,5),
+		MonedaID INT,
+		CostoContratoTotal DECIMAL(18,5),
+		TipoDeCambioID INT );
+GO
+
+CREATE PROCEDURE [dbo].[SP_AgregarCicloContrato]
+	@TVPContrato NuevoContrato READONLY,
+	@Inicio DATETIME2(7),
+	@Frecuencia INT,
+	@LocalProductorID INT,
+	@LocalProductorXContratoID INT
+AS 
+BEGIN
+	
+	SET NOCOUNT ON
+	
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT
+	DECLARE @Message VARCHAR(200)
+	DECLARE @InicieTransaccion BIT
+	DECLARE @ContratoID INT
+
+	SET @InicieTransaccion = 0
+	IF @@TRANCOUNT=0 BEGIN
+		SET @InicieTransaccion = 1
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+		BEGIN TRANSACTION		
+	END
+	
+	BEGIN TRY
+		SET @CustomError = 2001
+
+		INSERT INTO Contratos SELECT InicioVigencia, FinalVigencia, ProductorID, CostoContratoBruto, MonedaID, CostoContratoTotal, TipoDeCambioID 
+		FROM @TVPContrato
+		SET @ContratoID = @@IDENTITY
+		INSERT INTO CiclosDeRecoleccion (Inicio, Frecuencia, ContratoID, LocalProductorID, LocalProductorXContratoID)
+		VALUES (@Inicio, @Frecuencia, @ContratoID, @LocalProductorID, @LocalProductorXContratoID)
+
+		IF @InicieTransaccion=1 BEGIN
+			COMMIT
+		END
+	END TRY
+	BEGIN CATCH
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorState = ERROR_STATE()
+		SET @Message = ERROR_MESSAGE()
+		
+		IF @InicieTransaccion=1 BEGIN
+			ROLLBACK
+		END
+		RAISERROR('%s - Error Number: %i', 
+			@ErrorSeverity, @ErrorState, @Message, @CustomError)
+	END CATCH	
+END
+RETURN 0
+GO
+
+
+DECLARE @ContractTVP AS NuevoContrato;
+
+INSERT INTO @ContractTVP (InicioVigencia, FinalVigencia, ProductorID, CostoContratoBruto, MonedaID, CostoContratoTotal, TipoDeCambioID)
+   VALUES ('2023-04-25', '2023-12-25', 1, 150000, 1, 125000, 1)
+
+EXEC SP_AgregarCicloContrato @ContractTVP, '2023-04-25 10:00:00', 7, 1, 1;
+
+SELECT * FROM Contratos
+SELECT * FROM CiclosDeRecoleccion
